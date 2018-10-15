1c2423177daa4d8cc5f5c39bc2ce5146d039f05a
ritenojb <ritenojb@rose-hulman.edu>
2018-10-15 15:53:53 -0400

    DONE

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,17 +10,17 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jacob Ritenour.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_shortest_string()
+    #run_test_shortest_string()
     run_test_index_of_largest_number()
-    run_test_number_of_stutters()
-    run_test_is_palindrome()
-    run_test_count_same()
+    #run_test_number_of_stutters()
+    #run_test_is_palindrome()
+    #run_test_count_same()
 
 
 # ----------------------------------------------------------------------
@@ -103,10 +103,14 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
-
+    index_of_shortest = 0
+    for k in range(len(strings)):
+        if(len(strings[k])<len(strings[index_of_shortest])):
+            index_of_shortest = k
+    return strings[index_of_shortest]
 
 def run_test_index_of_largest_number():
     """ Tests the   index_of_largest_number   function. """
@@ -183,6 +187,10 @@ def index_of_largest_number(numbers, n):
     # TODO: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    index_of_max = 0
+    for k in range(n):
+        if(numbers[k]>numbers[index_of_max]):
+            index_of_max = k
 
 
 # ----------------------------------------------------------------------
