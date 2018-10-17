d96d1e3718f2a4a9fef22bcc96e4a0a802dede16
ritenojb <ritenojb@rose-hulman.edu>
2018-10-17 13:05:50 -0400

    DONE

--- src/m7_more_mutation.py
+++ src/m7_more_mutation.py
@@ -4,8 +4,8 @@ by changing the values of instance variables.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jacob Ritenour.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -169,13 +169,15 @@ def RETURN_circle(circle, color):
     where the color is either a string that Rosegraphics understands
     or a rg.Color object.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
     #
     # HINT: Use the   clone   method to make the copy.
-
+    circle2 = circle.clone()
+    circle2.fill_color = color
+    return circle2
 
 def run_test_MUTATE_circle():
     """ Tests the   MUTATE_circle   function. """
